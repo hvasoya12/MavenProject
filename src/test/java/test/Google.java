9765cb0d7cf1dac90daf5af87c5a79277f7a012b
hnpar <hnpar@10.0.0.3>
2021-06-24 17:59:29 -0400

    First Commit

---
+++ src/test/java/test/Google.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Google {
+	
+	@Test
+	public void titleValidation() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://google.com");
+		String title = driver.getTitle();
+		Assert.assertEquals(title, "Google");
+		driver.quit();
+		
+	}
+
+}
